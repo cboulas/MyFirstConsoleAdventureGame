ab2309187395f7e4683bcd17051d1aac425cbb23
Håvard Krüger <Havard@Kruger.im>
2018-01-22 14:37:20 +0100

    Moved combat functionality to seperate Combat.cs file

--- classes/Combat.cs
+++ classes/Combat.cs
@@ -4,6 +4,44 @@ namespace ConsoleAdventureGame
 {
     class Combat
     {
-        
+        public Combat(Character characterOne, Character characterTwo)
+        {
+            StartCombat(characterOne, characterTwo);
+        }
+
+        public static void StartCombat(Character characterOne, Character characterTwo)
+        {
+            while ((characterOne.hitPoints) > 0 && (characterTwo.hitPoints > 0))
+            {
+                int i = 1;
+                Console.WriteLine($"****Starting Round {i}****");
+                PerformCombatRound(characterOne, characterTwo);
+                Console.Write($"{characterOne.name} has {characterOne.hitPoints} health left.");
+                Console.WriteLine($"{characterTwo.name} has {characterTwo.hitPoints} health left.");
+                Console.WriteLine();
+                Console.WriteLine();
+                i++;
+            }
+
+            if (characterOne.hitPoints > 0)
+            {
+                Console.WriteLine($"{characterOne.name} survived the fight with {characterOne.hitPoints} health left");
+            }
+            else if(characterTwo.hitPoints > 0)
+             Console.WriteLine($"{characterTwo.name} survived the fight with {characterTwo.hitPoints} health left");
+            else
+                Console.WriteLine($"Both {characterOne.name} and {characterTwo.name} died.");
+        }
+
+        public static void PerformCombatRound(Character characterOne, Character characterTwo)
+        {
+            int characterOneDamage = characterOne.AttackDamage();
+            characterTwo.hitPoints -= characterOneDamage;
+            Console.WriteLine($"{characterOne.name} hits {characterTwo.name} for {characterOneDamage} damage.");
+
+            int characterTwoDamage = characterTwo.AttackDamage();
+            characterOne.hitPoints -= characterTwoDamage;
+            Console.WriteLine($"{characterTwo.name} hits {characterOne.name} for {characterTwoDamage}");
+        }
     }
 }
