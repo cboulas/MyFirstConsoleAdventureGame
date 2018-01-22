c9d949138b78535ff7ae4a1662ad7287ffaff38a
Håvard Krüger <Havard@Kruger.im>
2018-01-22 11:40:21 +0100

    Created .net Core Console Project.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAdventureGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
