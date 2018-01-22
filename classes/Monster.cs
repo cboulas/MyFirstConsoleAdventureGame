4598c4743f757afd5923e6fbd71a3b9b393e25ad
Håvard Krüger <Havard@Kruger.im>
2018-01-22 14:22:29 +0100

    Added combat functionality

--- classes/Monster.cs
+++ classes/Monster.cs
@@ -3,7 +3,7 @@ namespace ConsoleAdventureGame
     class Monster : Character
     {
 
-        public Monster(string name, int hitPoints, string weapon)
+        public Monster(string name, int hitPoints, Weapon weapon)
         {
             _name = name;
             _hitPoints = hitPoints;
--- classes/Player.cs
+++ classes/Player.cs
@@ -2,11 +2,14 @@ namespace ConsoleAdventureGame
 {
     class Player : Character
     {
-        public Player(string name, int hitPoints, string weapon)
+        
+        public Player(string name, int hitPoints, Weapon weapon, int minDamage, int maxDamage)
         {
             _name = name;
             _hitPoints = hitPoints;
             _weapon = weapon;
+            _minDamage = weapon.minDamage;
+            _maxDamage = weapon.maxDamage;
         }
     }
 
